f1dcaee68bec9bb42f308238c5e49f797fb9d0de
GL-19 <loiolagl19@gmail.com>
2022-09-13 17:46:43 -0300

    fix: corrected mongodb connection error, changing mongodb port from 27017:27017 to 8082:27017

--- src/main/java/com/springbootmongodbamigoscode/SpringBootMongodbAmigoscodeApplication.java
+++ src/main/java/com/springbootmongodbamigoscode/SpringBootMongodbAmigoscodeApplication.java
@@ -1,7 +1,14 @@
 package com.springbootmongodbamigoscode;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootMongodbAmigoscodeApplication {
@@ -10,4 +17,27 @@ public class SpringBootMongodbAmigoscodeApplication {
 		SpringApplication.run(SpringBootMongodbAmigoscodeApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(StudentRepository repository) {
+		return args -> {
+			Address address = new Address(
+					"USA",
+					"New York",
+					"55478956"
+			);
+
+			Student student =  new Student(
+					"Peter",
+					"Parker",
+					"parker@email.com",
+					Gender.MALE,
+					address,
+					List.of("Computer Science", "Mathematics"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+
+			repository.insert(student);
+		};
+	}
 }
