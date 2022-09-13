bb07d366a9c2cf77bb11390d0fc38122c526c7ad
GL-19 <loiolagl19@gmail.com>
2022-09-13 16:00:04 -0300

    feat: add docker-compose with mongodb and mongo-express

---
+++ src/main/java/com/springbootmongodbamigoscode/SpringBootMongodbAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmongodbamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongodbAmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongodbAmigoscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootmongodbamigoscode/SpringBootMongodbAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmongodbamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbAmigoscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
