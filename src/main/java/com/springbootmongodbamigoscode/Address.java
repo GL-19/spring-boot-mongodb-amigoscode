af84a5775005271058636c36bde3daba8f6ddcc1
GL-19 <loiolagl19@gmail.com>
2022-09-13 16:59:06 -0300

    feat: add classes Student, Address and enum Gender

---
+++ src/main/java/com/springbootmongodbamigoscode/Address.java
@@ -0,0 +1,10 @@
+package com.springbootmongodbamigoscode;
+
+import lombok.Data;
+
+@Data
+public class Address {
+    private String country;
+    private String city;
+    private String postCode;
+}
---
+++ src/main/java/com/springbootmongodbamigoscode/Gender.java
@@ -0,0 +1,5 @@
+package com.springbootmongodbamigoscode;
+
+public enum Gender {
+    MALE, FEMALE
+}
